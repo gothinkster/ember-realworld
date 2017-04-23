2652a2549c3397582d6b00055deaae19f1412928
Alex LaFroscia <alex@lafroscia.com>
2017-04-23 13:28:52 -0700

    Add Login and Register routes and templates

--- app/router.js
+++ app/router.js
@@ -8,6 +8,8 @@ const Router = Ember.Router.extend({
 
 Router.map(function() {
   this.route('home', { path: '/' });
+  this.route('login');
+  this.route('register');
 });
 
 export default Router;
