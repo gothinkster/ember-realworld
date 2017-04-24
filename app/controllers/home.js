253449ef929d7972774d766fbd5e59d0af3107bd
Alon Bukai <alonzorz@gmail.com>
2017-04-24 14:48:01 +0300

    Added authentication check for your feed in home

---
+++ app/controllers/home.js
@@ -0,0 +1,7 @@
+import Ember from "ember";
+const { computed, inject } = Ember;
+export default Ember.Controller.extend({
+    session: inject.service(),
+
+    isAuthenticated: computed.oneWay("session.isAuthenticated")
+});
