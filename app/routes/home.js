7be32e8fb22cce75edf3e91c2896b01dca4752cf
Alon Bukai <alonzorz@gmail.com>
2017-04-24 14:42:28 +0300

    Added component feed-item and used in home route

--- app/routes/home.js
+++ app/routes/home.js
@@ -1,3 +1,11 @@
-import Ember from 'ember';
+import Ember from "ember";
 
-export default Ember.Route.extend({});
+export default Ember.Route.extend({
+    model: function() {
+        return {
+            articleContent: Ember.String.htmlSafe(
+                "<h1>The song you won't ever stop singing. No matter how hard you try.</h1><p>This is the description for the post.</p><span>Read more...</span>"
+            )
+        };
+    }
+});
