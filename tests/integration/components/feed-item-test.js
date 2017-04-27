aa871b214db22adeccff4fa8e23351c73ff6f38e
Alon Bukai <alonzorz@gmail.com>
2017-04-27 18:14:24 +0300

    Fixed feed-item-test not contained yield broke the test

--- tests/integration/components/feed-item-test.js
+++ tests/integration/components/feed-item-test.js
@@ -12,15 +12,4 @@ test('it renders', function(assert) {
   this.render(hbs`{{feed-item}}`);
 
   assert.equal(this.$().text().trim(), '');
-
-  // Template block usage:
-  this.render(
-    hbs`
-    {{#feed-item}}
-      template block text
-    {{/feed-item}}
-  `
-  );
-
-  assert.equal(this.$().text().trim(), 'template block text');
 });
