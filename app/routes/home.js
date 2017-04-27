80eccb0282e944924e74549897025309d9dc6084
Alon Bukai <alonzorz@gmail.com>
2017-04-27 18:04:12 +0300

    Formatted tempates and removed redundant code from home.js

--- app/routes/home.js
+++ app/routes/home.js
@@ -1,11 +1,8 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
-  model: function() {
+  model() {
     return {
-      articleContent: Ember.String.htmlSafe(
-        '<h1>Want to help out?</h1><p>Check out our GitHub!</p><span>Go Now...</span>'
-      ),
       articles: this.store.findAll('article')
     };
   }
