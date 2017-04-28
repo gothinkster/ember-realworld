18390f9ec5695ddd0fc875640299ea09ca672e75
Alon Bukai <alonzorz@gmail.com>
2017-04-28 14:45:08 +0300

    Added component popular-tags and setup mock data in home route

--- app/routes/home.js
+++ app/routes/home.js
@@ -3,6 +3,7 @@ import Ember from 'ember';
 export default Ember.Route.extend({
   model() {
     return {
+      tagsTemp: ['emberjs', 'test', 'programming', 'javascript', 'angularjs', 'react', 'mean', 'node', 'rails'],
       articles: this.store.findAll('article')
     };
   }
