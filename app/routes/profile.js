e2234f6a94d66b379edb0aec6351870e927bfff6
Alon <alonzorz@gmail.com>
2017-04-29 16:14:31 +0300

    Created serializer for user and configured it. Started laying out dynamic content in profile page

--- app/routes/profile.js
+++ app/routes/profile.js
@@ -1,3 +1,7 @@
 import Ember from 'ember';
 
-export default Ember.Route.extend({});
+export default Ember.Route.extend({
+  model(params) {
+    return this.store.find('user', params.username);
+  }
+});
