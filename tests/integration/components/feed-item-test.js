b8b9eaa2c081e7549a18ca017ced08121aa78a70
Alon <alonzorz@gmail.com>
2017-04-29 15:26:43 +0300

    Built tests for feed-item component

--- tests/integration/components/feed-item-test.js
+++ tests/integration/components/feed-item-test.js
@@ -1,5 +1,6 @@
 import { moduleForComponent, test } from 'ember-qunit';
 import hbs from 'htmlbars-inline-precompile';
+import moment from 'moment';
 
 moduleForComponent('feed-item', 'Integration | Component | feed item', {
   integration: true
@@ -9,7 +10,43 @@ test('it renders', function(assert) {
   // Set any properties with this.set('myProperty', 'value');
   // Handle any actions with this.on('myAction', function(val) { ... });
 
-  this.render(hbs`{{feed-item}}`);
+  let article = {
+    author: {
+      image: 'https://static.productionready.io/images/smiley-cyrus.jpg',
+      username: 'Alon Bukai'
+    },
+    createdAt: moment('1995-12-25'),
+    favoritesCount: 9000,
+    body: 'This is feed item content',
+    slug: 'feed-item-title',
+    title: 'Feed Item Title'
+  };
 
-  assert.equal(this.$().text().trim(), '');
+  this.set('article', article);
+  this.render(
+    hbs`{{feed-item 
+            imageSrc=article.author.image 
+            author=article.author.username 
+            date=article.createdAt
+            hearts=article.favoritesCount 
+            content=article.body
+            slug=article.slug
+            title=article.title}}`
+  );
+
+  assert.equal(
+    this.$('img').attr('src'),
+    'https://static.productionready.io/images/smiley-cyrus.jpg',
+    'Image is default image'
+  );
+
+  assert.equal(this.$('a.author').text().trim(), 'Alon Bukai', 'Author name is correct');
+
+  assert.equal(this.$('span.date').text().trim(), 'Mon Dec 25 1995', 'Date is correct');
+
+  assert.equal(this.$('button.btn').text().trim(), '9000', 'Number of hearts is correct');
+
+  assert.equal(this.$('a.preview-link h1').text().trim(), 'Feed Item Title', 'Title is correct');
+
+  assert.equal(this.$('a.preview-link p').text().trim(), 'This is feed item content', 'Content is correct');
 });
