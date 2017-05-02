557d0a4839710a71888e83b82ff5a6854a5256b5
Alon <alonzorz@gmail.com>
2017-05-02 20:25:35 +0300

    Fixed linting error

--- tests/unit/adapters/user-test.js
+++ tests/unit/adapters/user-test.js
@@ -1,9 +1,13 @@
 import { moduleFor, test } from 'ember-qunit';
 
-moduleFor('adapter:user', 'Unit | Adapter | user', {
-  // Specify the other units that are required for this test.
-  // needs: ['serializer:foo']
-});
+moduleFor(
+  'adapter:user',
+  'Unit | Adapter | user',
+  {
+    // Specify the other units that are required for this test.
+    // needs: ['serializer:foo']
+  }
+);
 
 // Replace this with your real tests.
 test('it exists', function(assert) {
