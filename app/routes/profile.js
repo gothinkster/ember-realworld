5a04cd025d1568ac35dbe39360d8cd0ccd098c36
Alon <alonzorz@gmail.com>
2017-05-02 20:39:21 +0300

    Added author articles to profile page

--- app/routes/profile.js
+++ app/routes/profile.js
@@ -2,6 +2,13 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   model(params) {
-    return this.store.find('user', params.username);
+    return Ember.RSVP.hash({
+      user: this.store.find('user', params.username),
+      articles: this.store.query('article', { author: params.username })
+    });
+  },
+  setupController(controller, model) {
+    controller.set('articles', model.articles);
+    controller.set('user', model.user);
   }
 });
