6fce6f825a098e4e73fbc9982568d00c2e8d2c48
Jonathan Goldman <me@jonnii.com>
2017-05-03 15:31:49 -0400

    clean up

--- mirage/config.js
+++ mirage/config.js
@@ -1,10 +1,8 @@
 import { Response } from 'ember-cli-mirage';
 
 export default function() {
-  // this is awkward, ideally we would be doing this via the proxy.
-  // this.urlPrefix = 'https://conduit.productionready.io'; // make this `http://localhost:8080`, for example, if your API is on a different server
   this.namespace = 'api'; // make this `/api`, for example, if your API is namespaced
-  // this.timing = 400;      // delay for each request, automatically set to 0 during testing
+  this.timing = 400; // delay for each request, automatically set to 0 during testing
 
   this.post('/users/login', (schema, request) => {
     const attrs = JSON.parse(request.requestBody).user;
