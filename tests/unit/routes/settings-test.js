66fc19e956e49d5fb9a22e88d96f9dd0cfefa05f
Alon <alonzorz@gmail.com>
2017-05-05 19:50:34 +0300

    Fixed failing tests after authorization

--- tests/unit/routes/settings-test.js
+++ tests/unit/routes/settings-test.js
@@ -1,13 +1,10 @@
 import { moduleFor, test } from 'ember-qunit';
 
-moduleFor(
-  'route:settings',
-  'Unit | Route | settings',
-  {
-    // Specify the other units that are required for this test.
-    // needs: ['controller:foo']
-  }
-);
+moduleFor('route:settings', 'Unit | Route | settings', {
+  // Specify the other units that are required for this test.
+  // needs: ['controller:foo']
+  needs: ['service:session']
+});
 
 test('it exists', function(assert) {
   let route = this.subject();
