82bd4e74866375b3d6cd05d0269815c669faf0f1
Alon Bukai <alonzorz@gmail.com>
2017-05-05 09:54:35 +0300

    Added Edit Profile Settings Button to Profile

---
+++ tests/unit/controllers/profile-test.js
@@ -0,0 +1,16 @@
+import { moduleFor, test } from 'ember-qunit';
+
+moduleFor(
+  'controller:profile',
+  'Unit | Controller | profile',
+  {
+    // Specify the other units that are required for this test.
+    // needs: ['controller:foo']
+  }
+);
+
+// Replace this with your real tests.
+test('it exists', function(assert) {
+  let controller = this.subject();
+  assert.ok(controller);
+});
