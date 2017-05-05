df2178dc19a30f339df182481b2b05965c7eaf74
Alon Bukai <alonzorz@gmail.com>
2017-05-05 08:52:17 +0300

    Added currentUser to application controller Tried making this a service but had some troubles. We already have the user in the session so thats what I used.

--- app/controllers/application.js
+++ app/controllers/application.js
@@ -4,7 +4,7 @@ const { Controller, computed, inject, get } = Ember;
 
 export default Controller.extend({
   session: inject.service(),
-
+  currentUser: computed.oneWay('session.session.content.authenticated'),
   isAuthenticated: computed.oneWay('session.isAuthenticated'),
 
   actions: {
