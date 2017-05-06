ace0adccbbcfb55546a4eaf82cd4f4fef3bba401
Alon <alonzorz@gmail.com>
2017-05-06 17:54:57 +0300

    Setup Favorited Feed Item

--- app/components/feed-item.js
+++ app/components/feed-item.js
@@ -1,12 +1,3 @@
 import Ember from 'ember';
 
-const { computed } = Ember;
-
-export default Ember.Component.extend({
-  isFavorited: computed('article', {
-    get() {
-      //Stub until feed-item is pulled
-      return true;
-    }
-  })
-});
+export default Ember.Component.extend({});
