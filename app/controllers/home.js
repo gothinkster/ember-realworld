f4f0d0377ae4b36a6447bba9b2b7db9b2ece1b10
Alon Bukai <alonzorz@gmail.com>
2017-05-15 18:16:05 +0300

    Updating home for pagianation

--- app/controllers/home.js
+++ app/controllers/home.js
@@ -1,5 +1,6 @@
 import Ember from 'ember';
 const { computed, inject } = Ember;
+
 export default Ember.Controller.extend({
   session: inject.service(),
 
--- app/routes/home.js
+++ app/routes/home.js
@@ -1,10 +1,59 @@
 import Ember from 'ember';
+import { task, timeout } from 'ember-concurrency';
+
+const DEBUG = false;
+
+const { get } = Ember;
 
 export default Ember.Route.extend({
+  loadHorizon: 60,
+  pageSize: 20,
+  unloadHorizon: Infinity,
+
+  'on-observe': function(dataset) {
+    if (DEBUG) {
+      console.log('dataset =', dataset); // eslint-disable-line no-console
+    }
+  },
+
+  'timeout-ms': 5,
+
+  setReadOffset: task(function*(dataset, offset) {
+    yield timeout(get(this, 'timeout-ms'));
+    dataset.setReadOffset(offset);
+  }).restartable(),
   model() {
     return Ember.RSVP.hash({
       popularTags: this.store.findAll('tag'),
-      articles: this.store.findAll('article')
+      pageSize: get(this, 'pageSize'),
+      loadHorizon: get(this, 'loadHorizon'),
+      columns: get(this, 'columns')
+      // articles: this.store.findAll('article')
     });
+  },
+  actions: {
+    fetch: function(pageOffset, pageSize, stats) {
+      let params = {
+        offset: pageOffset,
+        limit: pageSize
+      };
+      return this.store.query('article', params).then(data => {
+        // stats.totalPages = data.get('meta.total') / pageSize;
+        return data.toArray();
+      });
+    },
+    initializeReadOffset(dataset) {
+      this.get('setReadOffset').perform(dataset, 0);
+    },
+
+    onObjectAt(dataset, index) {
+      this.get('setReadOffset').perform(dataset, index);
+    },
+
+    logDatasetState(dataset) {
+      if (DEBUG) {
+        console.log('dataset =', dataset); // eslint-disable-line no-console
+      }
+    }
   }
 });
