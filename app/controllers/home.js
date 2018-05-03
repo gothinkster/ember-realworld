b72bd8c463cfab27eb096e2981b37afe5aec35c3
Laura Kajpust <lkajpust@gmail.com>
2018-05-03 17:00:25 -0400

    Add action to change tag selection

--- app/controllers/home.js
+++ app/controllers/home.js
@@ -1,9 +1,20 @@
 import Controller from '@ember/controller';
-import { inject as service } from '@ember/service';
-import { oneWay } from '@ember/object/computed';
+import {
+  inject as service
+} from '@ember/service';
+import {
+  oneWay
+} from '@ember/object/computed';
 
 export default Controller.extend({
   session: service(),
 
-  isAuthenticated: oneWay('session.isAuthenticated')
+  isAuthenticated: oneWay('session.isAuthenticated'),
+  activeTag: null,
+
+  actions: {
+    setTag(tag) {
+      this.set('activeTag', tag);
+    }
+  }
 });
