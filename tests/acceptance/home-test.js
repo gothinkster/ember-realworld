cacc84376f1044b75af4ab12c524af1ac4a5a66b
Laura Kajpust <lkajpust@gmail.com>
2018-05-07 18:26:04 -0400

    Update test (the limit is 10, not 20)

--- tests/acceptance/home-test.js
+++ tests/acceptance/home-test.js
@@ -1,18 +1,27 @@
-import { module, test } from 'qunit';
-import { visit, currentURL, findAll } from '@ember/test-helpers';
-import { setupApplicationTest } from 'ember-qunit';
+import {
+  module,
+  test
+} from 'qunit';
+import {
+  visit,
+  currentURL,
+  findAll
+} from '@ember/test-helpers';
+import {
+  setupApplicationTest
+} from 'ember-qunit';
 import setupMirage from 'ember-cli-mirage/test-support/setup-mirage';
 
-module('Acceptance | home', function(hooks) {
+module('Acceptance | home', function (hooks) {
   setupApplicationTest(hooks);
   setupMirage(hooks);
 
-  test('visiting /', async function(assert) {
-    server.createList('article', 20);
+  test('visiting /', async function (assert) {
+    server.createList('article', 10);
 
     await visit('/');
 
     assert.equal(currentURL(), '/');
-    assert.equal(findAll('[data-test-article-preview]').length, 20);
+    assert.equal(find(testSelector('article-preview')).length, 10);
   });
 });
