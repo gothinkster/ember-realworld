fe8f5176d9012eb33c1fdd9ce0e8197814c6e254
Laura Kajpust <lkajpust@gmail.com>
2018-05-08 16:02:24 -0400

    Make posts per page a variable

--- app/components/ul-pagination.js
+++ app/components/ul-pagination.js
@@ -3,7 +3,7 @@ const { computed } = Ember;
 
 export default Ember.Component.extend({
   pageArray: computed('totalPages', function() {
-    let actualTotal = Math.ceil(this.get('totalPages') / 10);
+    let actualTotal = Math.ceil(this.get('totalPages') / this.get('limit'));
     if (actualTotal < 0) return ['1'];
     return Array.from(Array(actualTotal).keys()).map(num => ++num);
   }),
--- app/routes/home.js
+++ app/routes/home.js
@@ -14,12 +14,14 @@ export default Ember.Route.extend({
   },
 
   model(params) {
+    let perPage = 10;
     return Ember.RSVP.hash({
+      perPage: perPage,
       popularTags: this.store.findAll('tag'),
       articles: this.store.query('article', {
         tag: params.tag,
         limit: 10,
-        offset: (params.page - 1) * 10
+        offset: (params.page - 1) * perPage
       })
     });
   },
@@ -27,5 +29,6 @@ export default Ember.Route.extend({
   setupController(controller, model) {
     this._super(controller, model);
     controller.set('totalPages', model.articles.get('meta').articlesCount);
+    controller.set('postsPerPage', model.perPage);
   }
 });
