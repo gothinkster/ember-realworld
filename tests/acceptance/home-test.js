a562f8bb0393928fc977d36d0b528b890f55d99a
Laura Kajpust <lkajpust@gmail.com>
2018-05-08 15:12:01 -0400

    Update acceptance tests for home

--- tests/acceptance/home-test.js
+++ tests/acceptance/home-test.js
@@ -17,11 +17,45 @@ module('Acceptance | home', function (hooks) {
   setupMirage(hooks);
 
   test('visiting /', async function (assert) {
-    server.createList('article', 10);
+    server.createList('article', 20);
 
     await visit('/');
 
     assert.equal(currentURL(), '/');
     assert.equal(find(testSelector('article-preview')).length, 10);
+    assert.equal(find(testSelector('tag-item')).length, 7);
+    assert.equal(find(testSelector('page-link')).length, 2);
+  });
+});
+
+test('clicking a page', function (assert) {
+  server.createList('article', 20);
+
+  visit('/');
+  click('.page-item:nth-child(2) a');
+
+  andThen(() => {
+    assert.equal(currentURL(), '/?page=2');
+  });
+});
+
+test('clicking a tag', function (assert) {
+  visit('/');
+  click('.tag-list a:first-child');
+
+  andThen(() => {
+    assert.equal(currentURL(), '/?tag=emberjs');
+  });
+});
+
+test('clicking a page and tag', function (assert) {
+  server.createList('article', 20);
+
+  visit('/');
+  click('.tag-list a:first-child');
+  click('.page-item:nth-child(2) a');
+
+  andThen(() => {
+    assert.equal(currentURL(), '/?page=2&tag=emberjs');
   });
 });
