a7fc482161c3899186d3fb64861e48b9388be7e2
Alon Bukai <alon@camilyo.com>
2018-10-28 20:34:55 +0200

    Update computed usage and action

--- app/components/ul-pagination.js
+++ app/components/ul-pagination.js
@@ -2,10 +2,12 @@ import { computed } from '@ember/object';
 import Component from '@ember/component';
 
 export default Component.extend({
-  pageArray: computed('totalPages', function() {
-    const actualTotal = Math.ceil(this.get('totalPages') / this.get('limit'));
-    if (actualTotal < 0) return ['1'];
-    return Array.from(Array(actualTotal).keys()).map(num => ++num);
+  pageArray: computed('totalPages', {
+    get() {
+      const actualTotal = Math.ceil(this.get('totalPages') / this.get('limit'));
+      if (actualTotal < 0) return ['1'];
+      return Array.from(Array(actualTotal).keys()).map(num => ++num);
+    }
   }),
 
   actions: {
