494b964ea7aa38318d51dabd9e341391268f741e
Alon Bukai <alon@camilyo.com>
2018-11-04 20:32:15 +0200

    Fixed user serializer to allow registering

--- app/serializers/user.js
+++ app/serializers/user.js
@@ -1,12 +1,5 @@
 import DS from 'ember-data';
 
 export default DS.RESTSerializer.extend({
-  primaryKey: 'username',
-  normalizeResponse(store, primaryModelClass, payload, id, requestType) {
-    payload = {
-      user: payload.profile
-    };
-
-    return this._super(store, primaryModelClass, payload, id, requestType);
-  }
+  primaryKey: 'username'
 });
