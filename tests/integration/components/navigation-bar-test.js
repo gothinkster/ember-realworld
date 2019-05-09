143972489b5c3b922bc42c50f76be49e23c2a10f
Jonathan Goldman <me@jonnii.com>
2019-05-08 21:22:26 -0400

    Component for top navigation

---
+++ tests/integration/components/navigation-bar-test.js
@@ -0,0 +1,28 @@
+import { module, test } from 'qunit';
+import { setupRenderingTest } from 'ember-qunit';
+import { render } from '@ember/test-helpers';
+import hbs from 'htmlbars-inline-precompile';
+
+module('Integration | Component | navigation-bar', function(hooks) {
+  setupRenderingTest(hooks);
+
+  test('it renders signed out', async function(assert) {
+    await render(hbs`{{navigation-bar}}`);
+
+    assert.dom('[data-test-navigation-bar-sign-in]').exists();
+    assert.dom('[data-test-navigation-bar-editor-new]').doesNotExist();
+  });
+
+  test('it renders signed in', async function(assert) {
+    const session = {
+      isLoggedIn: true,
+    };
+
+    this.set('session', session);
+
+    await render(hbs`{{navigation-bar session=session}}`);
+
+    assert.dom('[data-test-navigation-bar-editor-new]').exists();
+    assert.dom('[data-test-navigation-bar-sign-in]').doesNotExist();
+  });
+});
