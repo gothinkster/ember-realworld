5430373cd0f02688f744fd3ec5b3ca495083e54f
Garrick Cheung <gcheung@fitbit.com>
2019-09-01 08:10:37 -0700

    Rename component `article-author-meta` to `article-meta` and add tests.

---
+++ app/components/article-meta.js
@@ -0,0 +1,13 @@
+import Component from '@ember/component';
+
+export default Component.extend({
+  /**
+   * Method executed when "favorite" button is clicked.
+   */
+  onFavoriteArticle() {},
+
+  /**
+   * Method executed when "follow" button is clicked.
+   */
+  onFollowAuthor() {},
+});
---
+++ tests/integration/components/article-meta-test.js
@@ -0,0 +1,88 @@
+import { module, test } from 'qunit';
+import { setupRenderingTest } from 'ember-qunit';
+import { click, render, settled } from '@ember/test-helpers';
+import hbs from 'htmlbars-inline-precompile';
+import { formatDate } from 'realworld-ember/helpers/format-date';
+
+module('Integration | Component | article-meta', function (hooks) {
+  setupRenderingTest(hooks);
+
+  const date = new Date();
+  const dateISOString = date.toISOString();
+  const article = {
+    author: {
+      image: 'https://static.productionready.io/images/smiley-cyrus.jpg',
+      id: 'AlonBukai',
+      following: false,
+    },
+    createdAt: dateISOString,
+    updatedAt: dateISOString,
+    favorited: false,
+    favoritesCount: 9000,
+    tagList: ['firstTag', 'secondTag', 'thirdTag'],
+    description: 'This is feed item description',
+    slug: 'feed-item-title',
+    title: 'Feed Item Title',
+  };
+
+  hooks.beforeEach(function () {
+    this.set('article', article);
+  });
+
+  test('it renders', async function (assert) {
+    await render(hbs`{{article-meta article=article}}`);
+
+    assert
+      .dom('[data-test-article-author-image]')
+      .hasAttribute('src', 'https://static.productionready.io/images/smiley-cyrus.jpg', 'Image is default image');
+
+    assert
+      .dom(`[data-test-article-author-username="${article.author.id}"]`)
+      .hasText('AlonBukai', 'Author name is correct');
+
+    assert.dom('[data-test-article-date]').hasText(formatDate([date]), 'Date is correct');
+
+    assert.dom('[data-test-follow-author-button]').includesText(`Follow ${article.author.id}`);
+
+    assert.dom('[data-test-favorite-article-button]').includesText(`Favorite Post`);
+
+    assert.dom('[data-test-article-favorites-count]').hasText('9000', 'Number of favorites is correct');
+  });
+
+  test('clicking on `follow-author` button triggers `onFollowAuthor` method', async function (assert) {
+    const onFollowAuthor = () => {
+      const isFollowing = this.get('article.author.following');
+      this.set('article.author.following', !isFollowing);
+    };
+
+    this.set('onFollowAuthor', onFollowAuthor);
+    await render(hbs`{{article-meta article=article onFollowAuthor=onFollowAuthor}}`);
+
+    await click('[data-test-follow-author-button]');
+    await settled();
+
+    assert.ok(this.get('article.author.following'), '`onFollowAuthor` method to be triggered');
+    assert
+      .dom('[data-test-follow-author-button]')
+      .includesText(`Unfollow ${article.author.id}`, 'Button text is updated');
+  });
+
+  test('clicking on `favorite-article` button triggers `onFollowAuthor` method', async function (assert) {
+    const onFavoriteArticle = () => {
+      const isFavorite = this.get('article.favorited');
+      const favoritesCount = this.get('article.favoritesCount');
+      this.set('article.favorited', !isFavorite);
+      this.set('article.favoritesCount', !isFavorite ? favoritesCount + 1 : favoritesCount - 1);
+    };
+
+    this.set('onFavoriteArticle', onFavoriteArticle);
+    await render(hbs`{{article-meta article=article onFavoriteArticle=onFavoriteArticle}}`);
+
+    await click('[data-test-favorite-article-button]');
+    await settled();
+
+    assert.ok(this.get('article.favorited'), '`onFavoriteArticle` method to be triggered');
+    assert.dom('[data-test-favorite-article-button]').includesText(`Unfavorite Post`, 'Button text is updated');
+    assert.dom('[data-test-article-favorites-count]').hasText('9001', 'Number of favorites is correct');
+  });
+});
