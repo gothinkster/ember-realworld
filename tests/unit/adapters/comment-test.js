060fb065d2061b12df879bf793310e82c08c6e12
Garrick Cheung <gcheung@fitbit.com>
2019-09-01 08:44:12 -0700

    Add tests for comment adapter.

--- tests/unit/adapters/comment-test.js
+++ tests/unit/adapters/comment-test.js
@@ -1,12 +1,46 @@
 import { module, test } from 'qunit';
 import { setupTest } from 'ember-qunit';
+import EmberObject from '@ember/object';
 
 module('Unit | Adapter | comment', function(hooks) {
   setupTest(hooks);
 
-  // Replace this with your real tests.
-  test('it exists', function(assert) {
+  const snapshot = {
+    record: EmberObject.create({
+      article: {
+        id: 1,
+      },
+    }),
+  };
+
+  test('`pathForType` method returns the expected type', function(assert) {
     const adapter = this.owner.lookup('adapter:comment');
-    assert.ok(adapter);
+
+    assert.equal(adapter.pathForType(), 'articles');
+    assert.expect(1);
+  });
+
+  test('`urlForCreateRecord` method returns expected URL', function(assert) {
+    const adapter = this.owner.lookup('adapter:comment');
+    const host = adapter.host;
+
+    assert.equal(adapter.urlForCreateRecord('comment', snapshot), `${host}/api/articles/1/comments`);
+    assert.expect(1);
+  });
+
+  test('`urlForUpdateRecord` method returns expected URL', function(assert) {
+    const adapter = this.owner.lookup('adapter:comment');
+    const host = adapter.host;
+
+    assert.equal(adapter.urlForUpdateRecord(1, 'comment', snapshot), `${host}/api/articles/1/comments/1`);
+    assert.expect(1);
+  });
+
+  test('`urlForDeleteRecord` method returns expected URL', function(assert) {
+    const adapter = this.owner.lookup('adapter:comment');
+    const host = adapter.host;
+
+    assert.equal(adapter.urlForDeleteRecord(1, 'comment', snapshot), `${host}/api/articles/1/comments/1`);
+    assert.expect(1);
   });
 });
