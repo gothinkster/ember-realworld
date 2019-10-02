f53cd782908b4c9a12063fd2e7f798234ce2f59a
Garrick Cheung <gcheung@fitbit.com>
2019-10-01 22:35:29 -0700

    Use `favorite-article-button` component to follow DDAU in `feed-item` component. - Change required adding action to controllers.

--- app/controllers/profile/index.js
+++ app/controllers/profile/index.js
@@ -1,12 +1,12 @@
 import Controller from '@ember/controller';
 import { inject as service } from '@ember/service';
+import { readOnly } from '@ember/object/computed';
 
 export default Controller.extend({
   session: service(),
-  queryParams: ['feed', 'tag', 'page'],
-  feed: null,
-  tag: null,
-  page: 1,
+
+  articles: readOnly('model.articles'),
+
   actions: {
     async favoriteArticle(article) {
       return article.get('favorited') ? article.unfavorite() : article.favorite();
--- tests/unit/controllers/profile/index-test.js
+++ tests/unit/controllers/profile/index-test.js
@@ -3,13 +3,33 @@ import { setupTest } from 'ember-qunit';
 import EmberObject from '@ember/object';
 import test from 'ember-sinon-qunit/test-support/test';
 
-module('Unit | Controller | home', function(hooks) {
+module('Unit | Controller | profile/index', function(hooks) {
   setupTest(hooks);
+
+  test('`articles` computed property is `readOnly` of `model.articles`', function(assert) {
+    assert.expect(2);
+
+    const controller = this.owner.lookup('controller:profile/index');
+    const model = {
+      articles: [{ id: 1 }, { id: 2 }],
+    };
+
+    controller.set('model', model);
+
+    assert.equal(
+      controller.articles,
+      model.articles,
+      '`articles` computed property should be the same array as `model.articles`',
+    );
+    assert.throws(() => {
+      controller.articles = [];
+    }, '`articles` computed property is read only');
+  });
 
   test('`favoriteArticle` method favorites/unfavorites an article', async function(assert) {
     assert.expect(4);
 
-    const controller = this.owner.lookup('controller:home');
+    const controller = this.owner.lookup('controller:profile/index');
     const articleFavorited = EmberObject.create({
       favorited: true,
       favorite: this.stub().resolves(),
