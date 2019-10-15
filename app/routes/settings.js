67f81395c5588e5fbd8cf733b0fbd3ab17889f67
Garrick Cheung <gcheung@fitbit.com>
2019-10-15 01:04:34 -0400

    Warn user if there are unsaved changes to settings and rollback if they decide to leave the page without saving.

--- app/routes/settings.js
+++ app/routes/settings.js
@@ -1,5 +1,6 @@
 import Route from '@ember/routing/route';
 import { inject as service } from '@ember/service';
+import window from 'ember-window-mock';
 
 export default Route.extend({
   session: service(),
@@ -11,5 +12,42 @@ export default Route.extend({
     if (!this.session.isLoggedIn) {
       return this.transitionTo('login');
     }
+  },
+
+  actions: {
+    /**
+     * If there are changes to the article, confirm with the user before transitioning.
+     * If user continues to transition routes, rollback the changes.
+     * Rollback changes will unload it from the store if the article is new.
+     */
+    async willTransition(transition) {
+      const {
+        session: { user },
+      } = this.controller;
+      let shouldRollbackAttributes = false;
+
+      /**
+       * If there are dirty attributes, comfirm with user whether they want to leave withou saving.
+       * Otherwise, just roll back the model attributes, which will unload it from the store.
+       */
+      if (user.hasDirtyAttributes) {
+        if (window.confirm('You have not created the article. Are you sure you want to leave?')) {
+          shouldRollbackAttributes = true;
+        } else {
+          transition.abort();
+        }
+      } else {
+        shouldRollbackAttributes = true;
+      }
+
+      if (!transition.isAborted && shouldRollbackAttributes) {
+        /**
+         * Rollback attributes after transition to prevent the article content in the form to flash.
+         */
+        await transition;
+
+        user.rollbackAttributes();
+      }
+    },
   },
 });
--- tests/unit/routes/settings-test.js
+++ tests/unit/routes/settings-test.js
@@ -1,9 +1,12 @@
 import { module } from 'qunit';
 import { setupTest } from 'ember-qunit';
 import test from 'ember-sinon-qunit/test-support/test';
+import EmberObject from '@ember/object';
+import window, { setupWindowMock } from 'ember-window-mock';
 
 module('Unit | Route | settings', function(hooks) {
   setupTest(hooks);
+  setupWindowMock(hooks);
 
   test('transitions anonymous users to `login` route', function(assert) {
     const route = this.owner.lookup('route:settings');
@@ -12,5 +15,77 @@ module('Unit | Route | settings', function(hooks) {
     route.beforeModel();
 
     assert.ok(transitionToStub.calledOnceWith('login'), 'Should transition to `login` route');
+  });
+
+  module('`willTransition` action', function() {
+    test('Confirms with user whether to transition when the user record has not been saved and has changes', async function(assert) {
+      assert.expect(4);
+
+      const route = this.owner.lookup('route:settings');
+      const controller = EmberObject.create({
+        session: EmberObject.create(),
+      });
+      const user = EmberObject.create({
+        rollbackAttributes: this.stub(),
+        hasDirtyAttributes: true,
+      });
+      const transition = {
+        abort: this.stub(),
+      };
+      const confirmStub = this.stub(window, 'confirm').returns(true);
+
+      route.set('controller', controller);
+      controller.session.set('user', user);
+
+      await route.actions.willTransition.call(route, transition);
+
+      // When user wants to transition with changes, rollback.
+      assert.ok(
+        user.rollbackAttributes.calledOnce,
+        'Should rollback attributes when user wants to transition without saving',
+      );
+      assert.notOk(
+        transition.abort.called,
+        'Transition abort should not be called when user wants to leave without saving',
+      );
+
+      user.rollbackAttributes.reset();
+      transition.abort.reset();
+      confirmStub.returns(false);
+      await route.actions.willTransition.call(route, transition);
+
+      // When user wants save changes, abort transition.
+
+      assert.notOk(user.rollbackAttributes.called, 'Should not rollback attributes when user wants to save');
+      assert.ok(transition.abort.calledOnce, 'Transition abort should be called when user wants to save');
+    });
+
+    test('Rolls back attributes when an user record does not have changed attributes', async function(assert) {
+      assert.expect(2);
+
+      const route = this.owner.lookup('route:settings');
+      const controller = EmberObject.create({
+        session: EmberObject.create(),
+      });
+      const user = EmberObject.create({
+        rollbackAttributes: this.stub(),
+        hasDirtyAttributes: false,
+      });
+      const transition = {
+        abort: this.stub(),
+      };
+      const confirmStub = this.stub(window, 'confirm');
+
+      route.set('controller', controller);
+      controller.session.set('user', user);
+
+      await route.actions.willTransition.call(route, transition);
+
+      assert.ok(
+        user.rollbackAttributes.calledOnce,
+        'Should rollback attributes when there are no changes - will unload a new record from the store',
+      );
+      assert.notOk(confirmStub.called, 'User confirmation should not have been initiated when there are no changes.');
+    });
   });
 });
