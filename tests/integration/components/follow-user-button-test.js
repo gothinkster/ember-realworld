8e57a8cb4de388341b9ec2d86cae6a8c3091b9db
John <johpol@users.noreply.github.com>
2019-11-15 02:43:17 -0500

    Follow user button component (#92)
    * Follow user button component
    
    * updated tag name and follow action
    
    * follow-user-button component integration test
    
    * Use follow-user-button component in block form
    
    * Removing unneeded statement and addition unit test case
    
    * followAction renamed to onClick
    
    * fix failing eslint / prettier tests
    
    * Fix failing prettier test
    
    * Fix failing prettier test
    
    * btn btn-sm class names added to component
    
    * passing in following property
    
    * Removing class names that are now part of the component
    
    * Restoring class names

---
+++ tests/integration/components/follow-user-button-test.js
@@ -0,0 +1,50 @@
+import { module } from 'qunit';
+import { setupRenderingTest } from 'ember-qunit';
+import { click, render } from '@ember/test-helpers';
+import hbs from 'htmlbars-inline-precompile';
+import test from 'ember-sinon-qunit/test-support/test';
+
+module('Integration | Component | follow-user-button', function(hooks) {
+  setupRenderingTest(hooks);
+
+  test('`onClick` action is triggered from clicking the button', async function(assert) {
+    assert.expect(1);
+
+    const onClick = this.stub();
+
+    this.set('onClick', onClick);
+
+    // Template block usage:
+    await render(hbs`
+      {{follow-user-button onClick=onClick}}
+    `);
+
+    await click('[data-test-follow-author-button]');
+
+    assert.ok(onClick.calledOnce, 'Clicking on button should trigger `onClick` action');
+  });
+
+  test('`class` arg is appended to button `class` attribute', async function(assert) {
+    assert.expect(2);
+
+    const classNames = 'foo bar';
+    const onClick = () => {};
+
+    this.setProperties({
+      classNames,
+      onClick,
+    });
+
+    // Template block usage:
+    await render(hbs`
+      {{#follow-user-button class=classNames onClick=onClick}}
+        template block text
+      {{/follow-user-button}}
+    `);
+
+    const buttonAssertion = assert.dom('[data-test-follow-author-button]');
+
+    buttonAssertion.hasClass('foo', 'Button `class` attribute should have `foo`');
+    buttonAssertion.hasClass('bar', 'Button `class` attribute should have `bar`');
+  });
+});
