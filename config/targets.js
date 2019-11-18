1ff200387485450e005957139d992a65b8a213eb
Alon Bukai <alonzorz@gmail.com>
2019-11-18 20:03:56 +0200

    chore: Set targets to chrome

--- config/targets.js
+++ config/targets.js
@@ -1,13 +1,6 @@
 'use strict';
 
-const browsers = ['last 1 Chrome versions', 'last 1 Firefox versions', 'last 1 Safari versions'];
-
-const isCI = !!process.env.CI;
-const isProduction = process.env.EMBER_ENV === 'production';
-
-if (isCI || isProduction) {
-  browsers.push('ie 11');
-}
+const browsers = ['last 1 Chrome versions'];
 
 module.exports = {
   browsers,
