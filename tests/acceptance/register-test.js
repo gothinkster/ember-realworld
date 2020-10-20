d20933ffc8959553a26cb261818b96eec46a355f
Marvin Arnold <marvin@geeky.rocks>
2020-10-20 07:39:56 -0500

    bug: Instantiate and set registration vars as tracked

---
+++ tests/acceptance/register-test.js
@@ -0,0 +1,43 @@
+import { module, test } from 'qunit';
+import faker from 'faker';
+import { visit, currentURL, fillIn, click, settled } from '@ember/test-helpers';
+import { setupApplicationTest } from 'ember-qunit';
+import setupMirage from 'ember-cli-mirage/test-support/setup-mirage';
+import { setupLoggedOutUser } from '../helpers/user';
+
+module('Acceptance | register', function(hooks) {
+  setupApplicationTest(hooks);
+  setupMirage(hooks);
+  setupLoggedOutUser(hooks);
+
+  test('successful registration', async function(assert) {
+    const user = {
+      name: 'Test User',
+      email: faker.internet.email(),
+      password: 'password123',
+    };
+
+    await visit('/register');
+
+    await fillIn('[data-test-register-name]', user.name);
+    await fillIn('[data-test-register-email]', user.email);
+    await fillIn('[data-test-register-password]', user.password);
+
+    await click('[data-test-register-button]');
+    await settled();
+
+    assert.equal(currentURL(), '/', 'URL after login is Home');
+    assert.dom('[data-test-nav-username]').hasText(user.name, 'Logged in username is shown');
+    assert.dom('[data-test-nav-new-post]').exists('Logged in nav is shown');
+    assert.dom('[data-test-nav-sign-up]').doesNotExist('Logged out nav is not shown');
+  });
+
+  test('visiting /register has link to /login', async function(assert) {
+    await visit('/register');
+
+    await click('[data-test-login-link]');
+    await settled();
+
+    assert.equal(currentURL(), '/login', 'URL after click is Login');
+  });
+});
